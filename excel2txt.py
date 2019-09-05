04d86d555ee7da7740e3daec5f099f5c532937ef
XGM <252040903@qq.com>
2019-09-05 21:07:14 +0800

    添加命令行参数--header,打印断言错误

--- excel2txt.py
+++ excel2txt.py
@@ -5,14 +5,14 @@ import sys
 import getopt
 import xlrd
 
-def create_file(name, rows, is_print):
+def create_file(name, rows, header, is_print):
     # remove old file
     if os.path.exists(name):
         os.remove(name)
     # print header
     row_num = len(rows)
     column_num = len(rows[0])
-    data_begin = 3 # data begin row
+    data_begin = header # data begin row
     fields = rows[0]
     types = rows[1]
     descs = rows[2]
@@ -37,34 +37,37 @@ def create_file(name, rows, is_print):
             return
     # out file
     file = open(name, "w", encoding="utf-16")
-    for r in range(row_num):
-        row_data = rows[r]
-        if is_print:
-            print(row_data)
-        # "\n"
-        if r > 0:
-            file.write("\n")
-        for c in range(column_num):
-            # "\t"
-            if c > 0:
-                file.write("\t")
-            # field data
-            type_name = types[c]
-            field_data = row_data[c]
-            if r < data_begin:
-                file.write(str(field_data))
-            else:
-                if type_name == "STRING":
-                    assert(type(field_data) == type(""))
-                    file.write(field_data)
-                elif type_name == "INT":
-                    assert(type(field_data) == type(0.0))
-                    file.write(str(int(field_data)))
-                elif  type_name == "FLOAT":
-                    assert(type(field_data) == type(0.0))
-                    file.write(str(round(field_data, 4)))
-    # close file
-    file.close()
+    try:
+        for r in range(row_num):
+            row_data = rows[r]
+            if is_print:
+                print(row_data)
+            # "\n"
+            if r > 0:
+                file.write("\n")
+            for c in range(column_num):
+                # "\t"
+                if c > 0:
+                    file.write("\t")
+                # field data
+                type_name = types[c]
+                field_data = row_data[c]
+                if r < data_begin:
+                    file.write(str(field_data))
+                else:
+                    if type_name == "STRING":
+                        assert (type(field_data) == str), "data is not a string type file: %s row=%d field_name=%s type_name=%s" % (name, r + 1, field_name, type_name)
+                        file.write(field_data)
+                    elif type_name == "INT":
+                        assert (type(field_data) == float), "data is not a number type file: %s row=%d field_name=%s type_name=%s" % (name, r + 1, field_name, type_name)
+                        assert (field_data - int(field_data) == 0), "data is not a int type file: %s row=%d field_name=%s type_name=%s" % (name, r + 1, field_name, type_name)
+                        file.write(str(int(field_data)))
+                    elif type_name == "FLOAT":
+                        assert (type(field_data) == float), "data is not a float type file: %s row=%d field_name=%s type_name=%s" % (name, r + 1, field_name, type_name)
+                        file.write(str(round(field_data, 4)))
+    finally:
+        # close file
+        file.close()
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -73,9 +76,10 @@ class Usage(Exception):
 def help():
     print("""
     Help info
-        -i, --input-dir        input file directory
-        -o, --output-dir       output file directory
-        -e, --filename-ext     filename extension, default is \".txt\"
+        -i, --input-dir         input file directory
+        -o, --output-dir        output file directory
+        -e, --filename-ext      filename extension, default is \".txt\"
+        --header                table header, default is 3
     """)
 
 def main(argv=None):
@@ -87,8 +91,9 @@ def main(argv=None):
         input_dir = "."
         output_dir = "."
         filename_ext = ".txt"
+        header = 3
         try:
-            opts, args = getopt.getopt(argv[1:], "hi:o:e:", ["help", "input-dir=", "output-dir=", "filename-ext="])
+            opts, args = getopt.getopt(argv[1:], "hi:o:e:", ["help", "input-dir=", "output-dir=", "filename-ext=", "header="])
             for opt_name, opt_value in opts:
                 if opt_name in ("-h", "--help"):
                     help()
@@ -99,6 +104,8 @@ def main(argv=None):
                     output_dir = opt_value
                 elif opt_name in ("-e", "--filename-ext"):
                     filename_ext = opt_value
+                elif opt_name == "--header":
+                    header = int(opt_value)
         except getopt.error as msg:
             raise Usage(msg)
 
@@ -119,20 +126,24 @@ def main(argv=None):
                     for i in range(nrows):
                         rows.append(sheet.row_values(i))
                     # check header
-                    if len(rows) < 3 or (rows[1][0] != "INT" and rows[1][0] != "STRING"):
+                    if len(rows) < header or (rows[1][0] != "INT" and rows[1][0] != "STRING"):
                         print("[%d] table: %s, sheet: %s, nrows: %d, ignore ! ! ! ! ! !" % (count, name, sheet.name, nrows))
                         continue
                     # create file
                     file_name = name.split(".", 1)[0]
                     if sheet.name.startswith("Sheet") == False:
                         file_name = sheet.name
-                    create_file(output_dir + "/" + file_name + filename_ext, rows, False)
+                    create_file(output_dir + "/" + file_name + filename_ext, rows, header, False)
                     #print("[%d] table: %s, sheet: %s, nrows: %d, ok." % (count, name, sheet.name, nrows))
     except Usage as err:
         print(err.msg)
         help()
         os.system("pause")
         return 2
+    except AssertionError as err:
+       print("Assertion:", err)
+       os.system("pause")
+       return 2 
     except:
        print("Unexpected error:", sys.exc_info()[0])
        os.system("pause")
